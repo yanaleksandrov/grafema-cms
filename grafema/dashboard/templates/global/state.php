2b2ec9f12abeb32b700c7babe99ade9d5829e414
yanaleksandrov <yanalexandrov@gmail.com>
2024-09-15 16:37:49 +0300

    UI fixes

--- grafema/dashboard/templates/global/state.php
+++ grafema/dashboard/templates/global/state.php
@@ -18,7 +18,7 @@ if ( ! defined( 'GRFM_PATH' ) ) {
 	new Sanitizer(
 		$args ?? [],
 		[
-			'class'       => 'class:m-auto t-center p-5 mw-320',
+			'class'       => 'class:dg jic m-auto t-center p-5 mw-320',
 			'title'       => 'trim',
 			'description' => 'trim',
 			'icon'        => 'id:empty-state-page',
